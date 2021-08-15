f07689a8dc99905685b562fa553bc45672406994
Bilqis <hirosasi01@gmail.com>
2021-08-15 20:48:37 +0700

    second commit

--- app/src/main/java/com/muflihunnisa/indoquiz/fragment/QuizFragment.kt
+++ app/src/main/java/com/muflihunnisa/indoquiz/fragment/QuizFragment.kt
@@ -17,13 +17,13 @@ import com.muflihunnisa.indoquiz.model.Quiz
 class QuizFragment : Fragment() {
     private val questions: MutableList<Quiz> = mutableListOf(
         Quiz(
-            image = "https://picsum.photos/200/300",
+            image = "http://kbu-cdn.com/dk/wp-content/uploads/kerak-telor.jpg",
             text = "Where is this food from?",
             answers = listOf("Betawi", "Jogja", "Surabaya", "Solo")
         ),
         Quiz(
             image = "https://ecs7.tokopedia.net/blog-tokopedia-com/uploads/2019/12/Hasyim-Asyari.jpg",
-            text = "what is the name of this hero??",
+            text = "What is the name of this hero??",
             answers = listOf("Hasyim Asyari", "Ir. Soekarno", "BJ Habibie", "Ki Hadjar Dewantara")
         ),
         Quiz(
@@ -32,12 +32,12 @@ class QuizFragment : Fragment() {
             answers = listOf("Kue Dongkal", "Klepon", "Kue Putu", "Risol")
         ),
         Quiz(
-            image = "https://picsum.photos/200/300",
+            image = "https://www.sembodorentcar.co.id/images/tour_package_detail/related/Candi_Borobudur1.jpg",
             text = "Which province is Yogyakarta in??",
             answers = listOf("Jawa Tengah", "Jawa Timur", "Jawa Barat", "Banten")
         ),
         Quiz(
-            image = "",
+            image = "https://cdn.idntimes.com/content-images/post/20200813/pixlr-20200813005845448-bf77501c890c4b693a180fee9ce50d19_600x400.jpg",
             text = "who is a pioneering figure in women's education from Bandung??",
             answers = listOf(
                 "Ratna Dewi Sartika",
@@ -52,7 +52,7 @@ class QuizFragment : Fragment() {
             answers = listOf("Gobak Sodor", "Enggrang", "Petak Umpet", "Lompat Tali")
         ),
         Quiz(
-            image = "",
+            image = "https://mmc.tirto.id/image/otf/500x0/2017/05/31/PPKI_ratio-16x9.jpg",
             text = "When was BPUPKI formed?",
             answers = listOf(
                 "29 April 1945",
@@ -62,22 +62,22 @@ class QuizFragment : Fragment() {
             )
         ),
         Quiz(
-            image = "",
+            image = "https://gurune.net/wp-content/uploads/2019/06/PEta2BIndonesia2BPropinsi2BEnglish.png",
             text = "How many provinces are there in Indonesia??",
             answers = listOf("34", "37", "32", "30")
         ),
         Quiz(
-            image = "",
+            image = "https://www.malukunews.co/cache/general/original/f/w/fwj1agp1cevq2c0j5rnxg.jpeg",
             text = "What is the typical food of the Maluku region?",
-            answers = listOf("woku komo-komo", "kerak telor", "sate kambing", "sate buntel")
+            answers = listOf("Woku komo-komo", "kerak telor", "sate kambing", "sate buntel")
         ),
         Quiz(
-            image = "",
+            image = "https://i0.wp.com/sebandung.com/wp-content/uploads/2015/03/Oray-orayan.jpg",
             text = "what is the game from the west java area?",
-            answers = listOf("oray-orayan", "Lompat tali", "bola bekel", "Patok Lele")
+            answers = listOf("Oray-orayan", "Lompat tali", "bola bekel", "Patok Lele")
         ),
         Quiz(
-            image = "",
+            image = "https://img.beritasatu.com/cache/beritasatu/600x350-2/411436861634.jpg",
             text = "What is the highest mountain in indonesia?",
             answers = listOf("Puncak Jaya", "Gunung Kerinci", "Gunung Papandayan", "Gunung Sumantri")
         )
@@ -121,9 +121,9 @@ class QuizFragment : Fragment() {
                     view.findNavController().navigate(R.id.action_quizFragment_to_loseFragment)
                 }
             }
+            Glide.with(this@QuizFragment).load(questions[questionIndex].image).centerCrop().into(quizBinding.ivPicture)
         }
         return quizBinding.root
-        Glide.with(this@QuizFragment).load(questions[questionIndex].image).centerCrop().into(quizBinding.ivPicture)
     }
 
     private fun randomQuestion() {
